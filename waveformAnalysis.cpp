e30bdcd48d579e50a2d7b66e9ae866ee89cb7ac3
Simone Pasquini <simo.pp07@gmail.com>
2025-07-22 17:39:26 +0200

    Fix initialisation of parameters

--- waveformAnalysis.cpp
+++ waveformAnalysis.cpp
@@ -5,48 +5,54 @@
 #include <iostream>
 #include <numeric>
 
-WaveformAnalysis::WaveformAnalysis(std::vector<double> const &s = {},
-                                   double ts = {}, double sp = {})
-    : fSamples{s}, fTimestamp{ts}, fSamplePeriod{sp} {
-  WaveformAnalysis::analyse();  // When a waveform is found, analyse it
+WaveformAnalysis::WaveformAnalysis(std::vector<double> const &s, double ts,
+                                   double sp)
+    : fSamples{s}, fTimestamp{ts}, fSamplePeriod{sp}
+{
+  WaveformAnalysis::analyse(); // When a waveform is found, analyse it
 }
 
-std::vector<double> const &WaveformAnalysis::getSamples() const {
+std::vector<double> const &WaveformAnalysis::getSamples() const
+{
   return fSamples;
 }
 double WaveformAnalysis::getTimestamp() const { return fTimestamp; }
 double WaveformAnalysis::getSamplePeriod() const { return fSamplePeriod; }
 double WaveformAnalysis::getBaseline() const { return fBaseline; }
-std::vector<Pulse> const &WaveformAnalysis::getPulses() const {
+std::vector<Pulse> const &WaveformAnalysis::getPulses() const
+{
   return fPulses;
 }
 
-void WaveformAnalysis::analyse() {
+void WaveformAnalysis::analyse()
+{
   WaveformAnalysis::computeBaseline();
   WaveformAnalysis::findAndIntegratePulse();
 }
 
-void WaveformAnalysis::computeBaseline(int nBaselineSamples = 50) {
+void WaveformAnalysis::computeBaseline(int nBaselineSamples)
+{
   fBaseline = std::accumulate(fSamples.begin(),
                               fSamples.begin() + nBaselineSamples, 0.) /
               nBaselineSamples;
 }
 
-double WaveformAnalysis::computeRMS(int nBaselineSamples = 50) {
+double WaveformAnalysis::computeRMS(int nBaselineSamples)
+{
   double mean = fBaseline;
   double sum{};
-  for (int i = 0; i < nBaselineSamples; i++) {
+  for (int i = 0; i < nBaselineSamples; i++)
+  {
     double delta = fSamples[i] - mean;
-    sum += delta * delta;  // Square the differences
+    sum += delta * delta; // Square the differences
   }
   return std::sqrt(sum / nBaselineSamples);
 }
 
-void WaveformAnalysis::findAndIntegratePulse(double tresholdFactor = 30.0,
-                                             double tolerance = 20,
-                                             int minWidth = 20,
-                                             int maxWidth = 20,
-                                             int minSeparation = 10) {
+void WaveformAnalysis::findAndIntegratePulse(double tresholdFactor,
+                                             double tolerance, int minWidth,
+                                             int maxWidth, int minSeparation)
+{
   double const noiseRMS = computeRMS(50);
 
   // Threshold for peak detection
@@ -58,24 +64,28 @@ void WaveformAnalysis::findAndIntegratePulse(double tresholdFactor = 30.0,
   // Used to find the higher value of the interval for pulseStart and pulseEnd
   double const upperLimit = fBaseline + tolerance * noiseRMS;
 
-  bool inPulse = false;  // Am I in pulse?
+  bool inPulse = false; // Am I in pulse?
   int pulseStart = 0;
   int pulseEnd = 0;
 
   // Min separation between pulseEnd of previous pulse and next pulse peak
   int lastPulseEnd = -minSeparation;
 
-  for (int i = 0; i < static_cast<int>(fSamples.size()) - 2; i++) {
+  for (int i = 0; i < static_cast<int>(fSamples.size()) - 2; i++)
+  {
     // Detection of maximum value
     if (!inPulse && i - lastPulseEnd >= minSeparation &&
         fSamples[i] > pulseThreshold && fSamples[i] > fSamples[i - 1] &&
-        fSamples[i] > fSamples[i + 1]) {
+        fSamples[i] > fSamples[i + 1])
+    {
       inPulse = true;
 
       // Try to find startPulse near baseline before the peak
-      bool foundStart = false;  // Have I found startPulse?
-      for (int j = i; j >= std::max(0, i - minWidth); --j) {
-        if (fSamples[j] >= lowerLimit && fSamples[j] <= upperLimit) {
+      bool foundStart = false; // Have I found startPulse?
+      for (int j = i; j >= std::max(0, i - minWidth); --j)
+      {
+        if (fSamples[j] >= lowerLimit && fSamples[j] <= upperLimit)
+        {
           pulseStart = j;
           foundStart = true;
           break;
@@ -83,13 +93,16 @@ void WaveformAnalysis::findAndIntegratePulse(double tresholdFactor = 30.0,
       }
     }
 
-    if (inPulse) {
+    if (inPulse)
+    {
       // Try to find endPulse near baseline after the peak
-      bool foundEnd = false;  // Have I found endPulse?
+      bool foundEnd = false; // Have I found endPulse?
       for (int j = i;
            j <= std::min(static_cast<int>(fSamples.size()) - 1, i + maxWidth);
-           ++j) {
-        if (fSamples[j] >= lowerLimit && fSamples[j] <= upperLimit) {
+           ++j)
+      {
+        if (fSamples[j] >= lowerLimit && fSamples[j] <= upperLimit)
+        {
           pulseEnd = j;
           foundEnd = true;
           break;
@@ -104,13 +117,14 @@ void WaveformAnalysis::findAndIntegratePulse(double tresholdFactor = 30.0,
   }
 
   // Limit case: still in pulse at the end
-  if (inPulse) {
+  if (inPulse)
+  {
     pulseEnd = fSamples.size() - 1;
     fPulses.push_back(integratePulse(pulseStart, pulseEnd));
   }
 
   // Print waveform's properties
-  std::cout << std::fixed << std::setprecision(1);  // Use 1 decimal digit
+  std::cout << std::fixed << std::setprecision(1); // Use 1 decimal digit
   std::cout << "\n\n=== PROPERTIES OF THE FOLLOWING WF ===\n";
   std::cout << "RMS           = " << noiseRMS << '\n';
   std::cout << "pulseThreshold= " << pulseThreshold << '\n';
@@ -119,19 +133,22 @@ void WaveformAnalysis::findAndIntegratePulse(double tresholdFactor = 30.0,
 }
 
 // Find area of 1 pulse
-Pulse WaveformAnalysis::integratePulse(int pulseStart, int pulseEnd) {
+Pulse WaveformAnalysis::integratePulse(int pulseStart, int pulseEnd)
+{
   int maxPulseId = pulseStart;
   double maxPulse = fSamples[pulseStart];
   double area{};
 
   // Find max of pulse
-  for (int i = pulseStart; i <= pulseEnd; i++) {
+  for (int i = pulseStart; i <= pulseEnd; i++)
+  {
     auto val = fSamples[i];
-    if (val > maxPulse) {
+    if (val > maxPulse)
+    {
       maxPulse = val;
       maxPulseId = i;
     }
-    area += fSamples[i] - fBaseline;  // Remove baseline
+    area += fSamples[i] - fBaseline; // Remove baseline
   }
   // Right dimensions of area in ADC*ns
   area *= fSamplePeriod;
--- waveformAnalysis.hpp
+++ waveformAnalysis.hpp
@@ -4,22 +4,25 @@
 #include <vector>
 
 // Define constants
-inline constexpr int nMinAnalysedRows{0};     // minimum index of analysed rows
-inline constexpr int nMaxAnalysedRows{9962};  // maximum rows
+inline constexpr int nMinAnalysedRows{0};    // minimum index of analysed rows
+inline constexpr int nMaxAnalysedRows{9930}; // maximum rows
 
 // Pulse struct allows to define the properties of pulses in a waveform
-struct Pulse {
-  double startTime{};  // Overall start time of the pulse
-  double endTime{};    // Overall end time of the pulse
-  double peakTime{};   // Overall time at which peak takes place
-  double peakValue{};  // Value of the pulse's peak in ADC
-  double area{};       // Area of pulse
+struct Pulse
+{
+  double startTime{}; // Overall start time of the pulse
+  double endTime{};   // Overall end time of the pulse
+  double peakTime{};  // Overall time at which peak takes place
+  double peakValue{}; // Value of the pulse's peak in ADC
+  double area{};      // Area of pulse
 };
 
 // Waveform class allows to perform waveform analysis
-class WaveformAnalysis {
- public:
-  WaveformAnalysis(std::vector<double> const &s, double ts, double sp);
+class WaveformAnalysis
+{
+public:
+  WaveformAnalysis(std::vector<double> const &s = {}, double ts = {},
+                   double sp = {});
 
   // Getters for waveform member data
 
@@ -40,18 +43,18 @@ class WaveformAnalysis {
 
   // Detect pulses
   void findAndIntegratePulse(double tresholdFactor = 30.0,
-                             double tolerance = 20, int minWidth = 3,
-                             int maxWidth = 5, int minSeparation = 10);
+                             double tolerance = 20, int minWidth = 20,
+                             int maxWidth = 20, int minSeparation = 10);
 
   // Find area of 1 pulse
   Pulse integratePulse(int pulseStart, int pulseEnd);
 
- private:
-  std::vector<double> fSamples{};  // Vector of samples forming the waveform
-  double fTimestamp{};             // Overall timestamp of the waveform
-  double fSamplePeriod{};          // Sampling period
-  double fBaseline{};              // Baseline of the waveform
-  std::vector<Pulse> fPulses{};    // Vector of pulses forming the waveform
+private:
+  std::vector<double> fSamples{}; // Vector of samples forming the waveform
+  double fTimestamp{};            // Overall timestamp of the waveform
+  double fSamplePeriod{};         // Sampling period
+  double fBaseline{};             // Baseline of the waveform
+  std::vector<Pulse> fPulses{};   // Vector of pulses forming the waveform
 };
 
 #endif
