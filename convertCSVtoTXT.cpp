4789d4910d2bda8f672fc9e13197fa780d9dd520
Simone Pasquini <simo.pp07@gmail.com>
2025-08-23 12:13:32 +0200

    Convert fastly .csv into .txt

---
+++ convertCSVtoTXT.cpp
@@ -0,0 +1,59 @@
+#include <filesystem>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+void convertCSVtoTXT(std::string const& folderPath) {
+  // Loop over files of directory
+  for (auto const& fileCSV : std::filesystem::directory_iterator(folderPath)) {
+    // Select file validity and .CSV extension
+    if (fileCSV.is_regular_file() && fileCSV.path().extension() == ".CSV") {
+      // Input file name opening
+      std::ifstream infile(fileCSV.path());
+      if (!infile.is_open()) {
+        std::cerr << "Failed to open " << fileCSV.path() << '\n';
+        return;
+      }
+
+      // Replace extension from .csv to .txt
+      std::filesystem::path txtPath = fileCSV.path();
+      txtPath.replace_extension(".txt");
+
+      // Create output .txt file
+      std::ofstream outFile(txtPath);
+      if (!outFile.is_open()) {
+        std::cerr << "Failed to create " << txtPath << '\n';
+        return;
+      }
+
+      // Read input file lines
+      std::string line;
+      while (std::getline(infile, line)) {
+        // Defining loop variables
+        std::stringstream ss(line);
+        std::string item;
+        bool first = true;
+
+        // Loop over columns
+        while (std::getline(ss, item, ';')) {
+          if (!first) {
+            outFile << '\t';
+          }
+          outFile << item;
+          first = false;
+        }
+        outFile << '\n';
+      }
+      outFile.close();
+
+      // Status
+      std::cout << "Converted: " << fileCSV.path() << " -> " << txtPath << '\n';
+    }
+  }
+}
+
+int main() {
+  convertCSVtoTXT("/mnt/c/Users/Simone/Desktop/Test");
+
+  return EXIT_SUCCESS;
+}
